13fcb90bd99ff6fa1fbbf9fe160efc45946ad78b
Minkyun Yun <80194344+min-guni@users.noreply.github.com>
2024-07-24 00:00:34 +0900

    init: spring boot

---
+++ room-service/src/main/kotlin/vottega/room_service/RoomServiceApplication.kt
@@ -0,0 +1,11 @@
+package vottega.room_service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RoomServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<RoomServiceApplication>(*args)
+}
