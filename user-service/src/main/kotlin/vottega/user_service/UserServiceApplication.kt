eba59cf1c46de553f7505d778e0b55f2ff6377f5
Minkyun Yun <80194344+min-guni@users.noreply.github.com>
2024-07-23 22:10:23 +0900

    init spring boot kotlin

---
+++ user-service/src/main/kotlin/vottega/user_service/UserServiceApplication.kt
@@ -0,0 +1,11 @@
+package vottega.user_service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UserServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<UserServiceApplication>(*args)
+}
---
+++ user-service/src/test/kotlin/vottega/user_service/UserServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package vottega.user_service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
