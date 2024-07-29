42f99b70179dc14e07556970aa7341872e6fb7e5
Minkyun Yun <80194344+min-guni@users.noreply.github.com>
2024-07-29 22:47:18 +0900

    feature: Room api 완성

--- room-service/src/main/kotlin/vottega/room_service/web/RoomResource.kt
+++ room-service/src/main/kotlin/vottega/room_service/web/RoomResource.kt
@@ -1,10 +1,13 @@
 package vottega.room_service.web
 
 import org.springframework.web.bind.annotation.*
+import vottega.room_service.dto.UpdateRoomRequestDTO
 import vottega.room_service.dto.RoomResponseDTO
 import vottega.room_service.dto.mapper.ParticipantMapper
 import vottega.room_service.dto.mapper.RoomMapper
+import vottega.room_service.dto.participantInfoDTO
 import vottega.room_service.service.RoomService
+import java.util.*
 
 @RestController("/api/room")
 class RoomResource(
@@ -18,7 +21,17 @@ class RoomResource(
     }
 
     @PatchMapping("/{roomId}")
-    fun updateRoom(@PathVariable roomId: Long, @RequestBody roomName: String?, @RequestBody status: String?): RoomResponseDTO {
-        return roomMapper.toRoomOutDTO(roomService.updateRoom(roomId, roomName, status))
+    fun updateRoom(@PathVariable roomId: Long, @RequestBody roomRequestDTO: UpdateRoomRequestDTO): RoomResponseDTO {
+        return roomMapper.toRoomOutDTO(roomService.updateRoom(roomId, roomRequestDTO.roomName, roomRequestDTO.status))
+    }
+
+    @PutMapping("/{roomId}/participants")
+    fun addParticipants(@PathVariable roomId: Long, @RequestBody participantInfoDTOs: List<participantInfoDTO>): RoomResponseDTO {
+        return roomMapper.toRoomOutDTO(roomService.addParticipant(roomId, participantInfoDTOs))
+    }
+
+    @DeleteMapping("/{roomId}/participants/{participantId}")
+    fun removeParticipant(@PathVariable roomId: Long, @PathVariable participantId: UUID): RoomResponseDTO {
+        return roomMapper.toRoomOutDTO(roomService.removeParticipant(roomId, participantId))
     }
 }
