9bfc49d6f02d440784a2791e89f992c9c98abcfb
Minkyun Yun <80194344+min-guni@users.noreply.github.com>
2024-08-04 15:34:52 +0900

    feature: 카프카 메세지 전송 adaptor 작성

--- room-service/src/main/kotlin/vottega/room_service/adaptor/impl/RoomProducerImpl.kt
+++ room-service/src/main/kotlin/vottega/room_service/adaptor/impl/RoomProducerImpl.kt
@@ -13,16 +13,22 @@ class RoomProducerImpl(
     private val kafkaTemplate: KafkaTemplate<String, ByteArray>,
     private val roomMapper: RoomMapper,
     private val participantMapper: ParticipantMapper,
-
     ) : RoomProducer {
     override fun roomEditMessageProduce(room: Room) {
         //TODO 방 수정 이벤트 발생
+        val roomOutDTO = roomMapper.toRoomOutDTO(room)
+        //TODO avro로 변환
 
-
+        //TODO kafka로 전송
+        kafkaTemplate.send("room", roomOutDTO.id.toString() ,roomOutDTO.toString().toByteArray())
     }
 
     override fun participantEditMessageProduce(participant: Participant) {
         //TODO 방에 있는 사람 수정 이벤트 발생
+        val participantInfoDTO = participantMapper.toParticipantResponseDTO(participant)
+        //TODO avro로 변환
 
+        //TODO kafka로 전송
+        kafkaTemplate.send("participant", participantInfoDTO.roomId.toString() ,participantInfoDTO.toString().toByteArray())
     }
 }
