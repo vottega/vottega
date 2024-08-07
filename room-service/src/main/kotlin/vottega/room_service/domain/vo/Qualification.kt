b6bf12264c648fa5053c7f40cbaf848f1ca4622e
Minkyun Yun <80194344+min-guni@users.noreply.github.com>
2024-08-07 23:52:24 +0900

    fix : 기존 Qualification 삭제

--- room-service/src/main/kotlin/vottega/room_service/domain/vo/Qualification.kt
+++
@@ -1,7 +0,0 @@
-package vottega.room_service.domain.vo
-
-import jakarta.persistence.Embeddable
-import vottega.room_service.domain.enumeration.ParticipantRole
-
-@Embeddable
-data class Qualification(val position : String, val role : ParticipantRole, val canVote : Boolean)
