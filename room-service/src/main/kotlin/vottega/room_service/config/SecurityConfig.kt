689a5bf276a2ac98c1b163bbffabe2f1e58447ed
Minkyun Yun <80194344+min-guni@users.noreply.github.com>
2024-08-13 21:49:13 +0900

    feature : security TODO 작성

--- room-service/src/main/kotlin/vottega/room_service/config/SecurityConfig.kt
+++ room-service/src/main/kotlin/vottega/room_service/config/SecurityConfig.kt
@@ -1,10 +1,14 @@
 package vottega.room_service.config.SecurityConfig
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 
 @Configuration
 @EnableWebSecurity
 class SecurityConfig {
-
+    @Bean
+    fun roomOwnerSecurity(){
+        //TODO: Implement security for room owner 
+    }
 }
