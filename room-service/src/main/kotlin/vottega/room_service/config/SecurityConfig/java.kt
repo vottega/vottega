919f176cb10f03fa1556bb5a12a3e49dc22d9d08
Minkyun Yun <80194344+min-guni@users.noreply.github.com>
2024-08-13 21:47:03 +0900

    feature : spring security start

---
+++ room-service/src/main/kotlin/vottega/room_service/config/SecurityConfig/java.kt
@@ -0,0 +1,10 @@
+package vottega.room_service.config.SecurityConfig
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+
+}
