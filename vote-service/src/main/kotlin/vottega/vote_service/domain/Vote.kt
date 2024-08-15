d45e8d98f61b05c0ca04a644a3bec98785d0d59c
Minkyun Yun <80194344+min-guni@users.noreply.github.com>
2024-08-15 14:16:19 +0900

    feature : 엔티티 변경

--- vote-service/src/main/kotlin/vottega/vote_service/domain/Vote.kt
+++ vote-service/src/main/kotlin/vottega/vote_service/domain/Vote.kt
@@ -1,8 +1,10 @@
 package vottega.vote_service.domain
 
 import jakarta.persistence.*
+import vottega.vote_service.domain.enum.VoteResultType
 import vottega.vote_service.domain.enum.VoteStatus
 import java.time.LocalDateTime
+import java.util.*
 
 @Entity
 class Vote (title : String, val roomId: Long) {
@@ -19,10 +21,56 @@ class Vote (title : String, val roomId: Long) {
         private set
     var createdAt : LocalDateTime? = null
         private set
-    var startTime : LocalDateTime? = null
+    var lastUpdatedAt : LocalDateTime? = null
         private set
-    var endTime : LocalDateTime? = null
+    var startedAt : LocalDateTime? = null
+        private set
+    var finishedAt : LocalDateTime? = null
         private set
     var status : VoteStatus = VoteStatus.CREATED
         private set
+
+
+    fun startVote() {
+        if (status == VoteStatus.CREATED) {
+            status = VoteStatus.STARTED
+            startedAt = LocalDateTime.now()
+        } else {
+            throw IllegalStateException("이미 시작된 투표입니다.")
+        }
+    }
+
+    fun endVote() {
+        if (status == VoteStatus.STARTED) {
+            status = VoteStatus.ENDED
+            finishedAt = LocalDateTime.now()
+        } else {
+            throw IllegalStateException("아직 시작되지 않은 투표입니다.")
+        }
+    }
+
+    fun addVotePaper(userId : UUID, voteResultType: VoteResultType) {
+        if(status != VoteStatus.STARTED) {
+            throw IllegalStateException("투표가 시작되지 않았습니다.")
+        }
+        //이미 투표한 사람인지 확인
+        votePapers.forEach() {
+            if (it.userId == userId) {
+                throw IllegalStateException("이미 투표한 사람입니다.")
+            }
+        }
+
+        votePapers.add(VotePaper(userId, this, voteResultType))
+    }
+
+    @PrePersist
+    fun prePersist() {
+        createdAt = LocalDateTime.now()
+        lastUpdatedAt = LocalDateTime.now()
+    }
+
+    @PreUpdate
+    fun preUpdate() {
+        lastUpdatedAt = LocalDateTime.now()
+    }
 }
--- vote-service/src/main/kotlin/vottega/vote_service/domain/VotePaper.kt
+++ vote-service/src/main/kotlin/vottega/vote_service/domain/VotePaper.kt
@@ -1,25 +1,25 @@
 package vottega.vote_service.domain
 
 import jakarta.persistence.*
+import vottega.vote_service.domain.enum.VoteResultType
 import java.time.LocalDateTime
 import java.util.UUID
 
 @Entity
-class VotePaper (userId : UUID , vote: Vote) {
+class VotePaper(
+    val userId: UUID,
+    @ManyToOne @JoinColumn(name = "vote_id") val vote: Vote,
+    val voteResultType: VoteResultType
+) {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null
 
-    var userId : UUID = userId
+    var createdAt: LocalDateTime? = null
         private set
 
-    //TODO 찬 반 기
-
-    @ManyToOne
-    @JoinColumn(name = "vote_id")
-    var vote : Vote = vote
-        private set
-
-    var createdAt : LocalDateTime? = null
-        private set
+    @PrePersist
+    fun prePersist() {
+        createdAt = LocalDateTime.now()
+    }
 }
