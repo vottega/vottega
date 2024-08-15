a75aaa6c4738f31f6efc0e45bd80c46e4a63de33
Minkyun Yun <80194344+min-guni@users.noreply.github.com>
2024-08-15 16:57:37 +0900

    feature : Vote용 Enum 생성

---
+++ vote-service/src/main/kotlin/vottega/vote_service/domain/enum/VotePaperType.kt
@@ -0,0 +1,5 @@
+package vottega.vote_service.domain.enum
+
+enum class VotePaperType {
+    YES, NO, ABSTAIN, NOT_VOTED
+}
--- vote-service/src/main/kotlin/vottega/vote_service/domain/enum/VoteResultType.kt
+++ vote-service/src/main/kotlin/vottega/vote_service/domain/enum/VoteResultType.kt
@@ -1,5 +1,5 @@
 package vottega.vote_service.domain.enum
 
 enum class VoteResultType {
-    YES, NO, ABSTAIN
+    PASSED, REJECTED
 }
