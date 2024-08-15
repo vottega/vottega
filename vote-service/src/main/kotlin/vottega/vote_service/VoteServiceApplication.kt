c73bb20fa5412b934b160090211d6fefe6b15a7c
Minkyun Yun <80194344+min-guni@users.noreply.github.com>
2024-08-15 16:57:57 +0900

    feature : Feign Client Annotation 붙이기

--- vote-service/src/main/kotlin/vottega/vote_service/VoteServiceApplication.kt
+++ vote-service/src/main/kotlin/vottega/vote_service/VoteServiceApplication.kt
@@ -2,8 +2,10 @@ package vottega.vote_service
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication
+@EnableFeignClients
 class VoteServiceApplication
 
 fun main(args: Array<String>) {
