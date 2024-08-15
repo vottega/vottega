ac00d058c5c1ead78a3d2ee2f81460bbdd8c9104
Minkyun Yun <80194344+min-guni@users.noreply.github.com>
2024-08-15 14:17:06 +0900

    feature: VoteService 구현 및 수정

--- vote-service/src/main/kotlin/vottega/vote_service/service/VoteService.kt
+++ vote-service/src/main/kotlin/vottega/vote_service/service/VoteService.kt
@@ -1,8 +1,11 @@
 package vottega.vote_service.service
 
+import vottega.vote_service.domain.enum.VoteResultType
+import java.util.*
+
 interface VoteService {
     fun createVote(title : String, roomId: Long)
     fun startVote(voteId: Long)
     fun endVote(voteId: Long)
-    fun vote(voteId: Long, userId: Long, voteResultType: String)
+    fun addVotePaper(voteId: Long, userId: UUID, voteResultType: VoteResultType)
 }
--- vote-service/src/main/kotlin/vottega/vote_service/service/impl/VoteServiceImpl.kt
+++ vote-service/src/main/kotlin/vottega/vote_service/service/impl/VoteServiceImpl.kt
@@ -1,23 +1,35 @@
 package vottega.vote_service.service.impl
 
+import jakarta.transaction.Transactional
 import org.springframework.stereotype.Service
+import vottega.vote_service.domain.Vote
+import vottega.vote_service.domain.enum.VoteResultType
+import vottega.vote_service.repository.VoteRepository
 import vottega.vote_service.service.VoteService
+import java.util.*
 
 @Service
-class VoteServiceImpl : VoteService {
+@Transactional
+class VoteServiceImpl(
+    private val voteRepository: VoteRepository
+) : VoteService {
     override fun createVote(title: String, roomId: Long) {
-        TODO("Not yet implemented")
+        val vote = Vote(title = title, roomId = roomId)
+        voteRepository.save(vote)
     }
 
     override fun startVote(voteId: Long) {
-        TODO("Not yet implemented")
+        val vote = voteRepository.findById(voteId).orElseThrow { IllegalArgumentException("Vote not found") }
+        vote.startVote()
     }
 
     override fun endVote(voteId: Long) {
-        TODO("Not yet implemented")
+        val vote = voteRepository.findById(voteId).orElseThrow { IllegalArgumentException("Vote not found") }
+        vote.endVote()
     }
 
-    override fun vote(voteId: Long, userId: Long, voteResultType: String) {
-        TODO("Not yet implemented")
+    override fun addVotePaper(voteId: Long, userId: UUID, voteResultType: VoteResultType) {
+        val vote = voteRepository.findById(voteId).orElseThrow { IllegalArgumentException("Vote not found") }
+        vote.addVotePaper(userId, voteResultType)
     }
 }
