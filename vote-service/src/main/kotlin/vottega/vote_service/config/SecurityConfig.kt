05b2460485540959f28640f29458467f91295177
Minkyun Yun <80194344+min-guni@users.noreply.github.com>
2024-08-29 23:52:33 +0900

    feature : vote security init

---
+++ vote-service/src/main/kotlin/vottega/vote_service/config/SecurityConfig.kt
@@ -0,0 +1,13 @@
+package vottega.vote_service.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.web.SecurityFilterChain
+
+@EnableWebSecurity
+class SecurityConfig {
+    @Bean
+    fun canVoteSecurity(): SecurityFilterChain {
+        //vote 할 수 있는거 체크
+    }
+}
