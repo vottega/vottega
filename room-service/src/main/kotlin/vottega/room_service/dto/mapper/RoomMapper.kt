d8026f1d715767fa82e76c41bc8297ac98bbd3d2
Minkyun Yun <80194344+min-guni@users.noreply.github.com>
2024-09-22 23:28:46 +0900

    feature : avro 파일 변경 및 매퍼 개발

--- room-service/src/main/kotlin/vottega/room_service/dto/mapper/RoomMapper.kt
+++ room-service/src/main/kotlin/vottega/room_service/dto/mapper/RoomMapper.kt
@@ -3,6 +3,8 @@ package vottega.room_service.dto.mapper
 import org.springframework.stereotype.Component
 import vottega.room_service.domain.Room
 import vottega.room_service.dto.RoomResponseDTO
+import vottega.room_service.avro.RoomResponseDTO as AvroRoomResponseDTO
+import vottega.room_service.avro.ParticipantResponseDTO as AvroParticipantResponseDTO
 
 @Component
 class RoomMapper {
@@ -16,5 +18,19 @@ class RoomMapper {
             startedAt = room.startedAt,
             finishedAt = room.finishedAt
         )
+    }
+
+    fun toAvro(roomResponseDTO: RoomResponseDTO): AvroRoom {
+        return AvroRoomResponseDTO.newBuilder()
+            .setId(roomResponseDTO.id)
+            .setName(roomResponseDTO.name)
+            .setParticipants(
+                roomResponseDTO.participants.map { toAvro(it) }
+            )
+            .setCreatedAt(roomResponseDTO.createdAt.toInstant(ZoneOffset.UTC).toEpochMilli())
+            .setLastUpdatedAt(roomResponseDTO.lastUpdatedAt.toInstant(ZoneOffset.UTC).toEpochMilli())
+            .setStartedAt(roomResponseDTO.startedAt?.toInstant(ZoneOffset.UTC)?.toEpochMilli())
+            .setFinishedAt(roomResponseDTO.finishedAt?.toInstant(ZoneOffset.UTC)?.toEpochMilli())
+            .build()
     }
 }
