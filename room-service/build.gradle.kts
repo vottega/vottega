da61cbeadb521a08b52639e7cda9b8bc95f54866
Minkyun Yun <80194344+min-guni@users.noreply.github.com>
2024-09-24 23:08:06 +0900

    feature : Avro 관련 라이브러리 추가

--- room-service/build.gradle.kts
+++ room-service/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
 	kotlin("plugin.spring") version "1.9.24"
 	id("org.springframework.boot") version "3.3.2"
 	id("io.spring.dependency-management") version "1.1.6"
+	id("com.github.davidmc24.gradle.plugin.avro") version "1.3.0"
 	kotlin("plugin.jpa") version "1.9.24"
 }
 
@@ -52,6 +53,7 @@ dependencies {
 	testImplementation("org.springframework.kafka:spring-kafka-test")
 	testImplementation("org.springframework.security:spring-security-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	implementation("org.apache.avro:avro")
 }
 
 kotlin {
@@ -62,4 +64,9 @@ kotlin {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+avro {
+	fieldVisibility.set("PUBLIC_DEPRECATED")
+	stringType.set("String")
 }
