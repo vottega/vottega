408ef5bb44376631662e46c9af0e1a6c910d0544
Minkyun Yun <80194344+min-guni@users.noreply.github.com>
2024-09-26 23:40:00 +0900

    feature : room 입퇴장 처리 psuedo

--- room-service/src/main/kotlin/vottega/room_service/adaptor/RoomConsumer.kt
+++ room-service/src/main/kotlin/vottega/room_service/adaptor/RoomConsumer.kt
@@ -2,13 +2,16 @@ package vottega.room_service.adaptor
 
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Service
+import vottega.room_service.service.RoomService
 
 @Service
-class RoomConsumer(
+class RoomConsumer(val roomService: RoomService
 ) {
     @KafkaListener(topics = ["room"])
     fun participantConnectEventConsume(message: String) {
         //TODO 사람 입장 및 퇴장 이벤트 처리 (From Connection Service)
-        
+        roomService.exitParticipant(message)
+        roomService.enterParticipant(message)
+        //카프카 좀 더 배워서 실행
     }
 }
