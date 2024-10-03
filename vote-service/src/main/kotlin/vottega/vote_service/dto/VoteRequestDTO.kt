1d10ca2e89cc6519c7c643d7a5f3c1478555397c
Minkyun Yun <80194344+min-guni@users.noreply.github.com>
2024-10-03 23:58:49 +0900

    fix : dto 필드 값 추가

--- vote-service/src/main/kotlin/vottega/vote_service/dto/VoteRequestDTO.kt
+++ vote-service/src/main/kotlin/vottega/vote_service/dto/VoteRequestDTO.kt
@@ -1,3 +1,15 @@
 package vottega.vote_service.dto
 
-data class VoteRequestDTO(val title : String, val passRateNumerator: Int?, val passRateDenominator: Int?, val isSecret : Boolean?)
+import vottega.vote_service.domain.FractionVO
+import java.time.LocalDateTime
+
+data class VoteRequestDTO(
+    val agendaName: String,
+    val voteName: String,
+    val passRateNumerator: Int?,
+    val passRateDenominator: Int?,
+    val isSecret: Boolean?,
+    val reservedStartTime: LocalDateTime?,
+    val minParticipantNumber : Int?,
+    val minParticipantRate: FractionVO
+)
