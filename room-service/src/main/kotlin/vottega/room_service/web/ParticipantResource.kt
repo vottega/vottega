ad2d7f0b033b8af6529f44be373c4ba34da1ca15
Minkyun Yun <80194344+min-guni@users.noreply.github.com>
2024-10-16 23:45:17 +0900

    feature : Participant Controller Swagger 추가

--- room-service/src/main/kotlin/vottega/room_service/web/ParticipantResource.kt
+++ room-service/src/main/kotlin/vottega/room_service/web/ParticipantResource.kt
@@ -9,22 +9,26 @@ import java.util.*
 
 @RestController
 @RequestMapping("/api/room")
+@Tag(name = "Participant Controller", description = "참가자 관련 API")
 class ParticipantResource(
     private val roomService: RoomService,
     private val roomMapper: RoomMapper
 ) {
     @PutMapping("/{roomId}/participants")
+    @Operation(summary = "참가자 추가", description = "방에 참가자를 추가합니다.")
     fun addParticipants(
         @PathVariable roomId: Long,
         @RequestBody ParticipantInfoDTOList: List<ParticipantInfoDTO>
     ): RoomResponseDTO = roomService.addParticipant(roomId, ParticipantInfoDTOList)
 
     @DeleteMapping("/{roomId}/participants/{participantId}")
+    @Operation(summary = "참가자 삭제", description = "방에서 참가자를 삭제합니다.")
     fun removeParticipant(@PathVariable roomId: Long, @PathVariable participantId: UUID): RoomResponseDTO =
         roomService.removeParticipant(roomId, participantId)
 
 
     @PatchMapping("/{roomId}/participants/{participantId}")
+    @Operation(summary = "참가자 정보 수정", description = "참가자 정보를 수정합니다.")
     fun updateParticipant(
         @PathVariable roomId: Long,
         @PathVariable participantId: UUID,
