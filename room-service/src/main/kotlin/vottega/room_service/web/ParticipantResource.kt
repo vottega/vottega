2746144866a18879c47a86631d6c0fd51d672ade
Minkyun Yun <80194344+min-guni@users.noreply.github.com>
2024-10-16 23:46:48 +0900

    feature : import 추가

--- room-service/src/main/kotlin/vottega/room_service/web/ParticipantResource.kt
+++ room-service/src/main/kotlin/vottega/room_service/web/ParticipantResource.kt
@@ -1,5 +1,7 @@
 package vottega.room_service.web
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.*
 import vottega.room_service.dto.RoomResponseDTO
 import vottega.room_service.dto.mapper.RoomMapper
