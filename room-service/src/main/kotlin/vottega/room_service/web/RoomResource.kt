1bbee3b79a5d5420a6318390da5c4ca66287618e
Minkyun Yun <80194344+min-guni@users.noreply.github.com>
2024-10-16 23:46:12 +0900

    feature : RoomController Swagger 추가

--- room-service/src/main/kotlin/vottega/room_service/web/RoomResource.kt
+++ room-service/src/main/kotlin/vottega/room_service/web/RoomResource.kt
@@ -9,26 +9,32 @@ import vottega.room_service.service.RoomService
 
 @RestController
 @RequestMapping("/api/room")
+@Tag(name = "Room Controller", description = "방 관련 API")
 class RoomResource(
     private val roomService: RoomService
 ) {
     @GetMapping("/{roomId}")
+    @Operation(summary = "방 조회", description = "방을 조회합니다.")
     fun getRoom(@PathVariable roomId: Long): RoomResponseDTO =
         roomService.getRoom(roomId)
 
     @PatchMapping("/{roomId}")
+    @Operation(summary = "방 정보 수정", description = "방 정보를 수정합니다.")
     fun updateRoom(@PathVariable roomId: Long, @RequestBody roomRequestDTO: UpdateRoomRequestDTO): RoomResponseDTO =
         roomService.updateRoom(roomId, roomRequestDTO.roomName, roomRequestDTO.status)
 
     @PutMapping("/{roomId}/role")
+    @Operation(summary = "역할 추가", description = "방에 역할을 추가합니다.")
     fun putRole(@PathVariable roomId: Long, @RequestBody roleInfo: ParticipantRoleDTO): RoomResponseDTO =
         roomService.addRole(roomId, roleInfo)
 
     @DeleteMapping("/{roomId}/role/{role}")
+    @Operation(summary = "역할 삭제", description = "방에서 역할을 삭제합니다.")
     fun deleteRole(@PathVariable roomId: Long, @PathVariable role: String): RoomResponseDTO =
         roomService.deleteRole(roomId, role)
 
     @GetMapping("/list/{userId}")
+    @Operation(summary = "방 목록 조회", description = "사용자의 방 목록을 조회합니다.")
     fun getRoomList(@PathVariable userId: Long): List<RoomResponseDTO> = roomService.getRoomList(userId)
 
 
