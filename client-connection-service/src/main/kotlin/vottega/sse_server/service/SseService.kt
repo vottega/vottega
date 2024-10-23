e87fdf934a202713c430103e322eac06aeaee1e4
Minkyun Yun <80194344+min-guni@users.noreply.github.com>
2024-10-23 21:22:28 +0900

    feature : SseService BroadCast기능 추가

--- client-connection-service/src/main/kotlin/vottega/sse_server/service/SseService.kt
+++ client-connection-service/src/main/kotlin/vottega/sse_server/service/SseService.kt
@@ -1,15 +1,14 @@
 package vottega.sse_server.service
 
+import reactor.core.publisher.FluxSink
 import reactor.core.publisher.Mono
 import java.lang.Void
 import java.util.UUID
 
 interface SseService {
-    fun enterRoom(roomId: Long, userId : UUID)
-    fun exitRoom(roomId: Long, userId : UUID)
+    fun enterRoom(roomId: Long, userId : UUID, sink : FluxSink<String>)
+    fun exitRoom(roomId: Long, userId : UUID , sink : FluxSink<String>)
 
-    fun consumeShortHand(): Mono<String>
-    fun consumeVoteInfo(): Mono<String>
-    fun consumeRoomInfo(): Mono<String>
+    fun broadcastToRoom(roomId: Long, message: String)
 
 }
--- client-connection-service/src/main/kotlin/vottega/sse_server/service/impl/SseServiceImpl.kt
+++ client-connection-service/src/main/kotlin/vottega/sse_server/service/impl/SseServiceImpl.kt
@@ -1,18 +1,28 @@
 package vottega.sse_server.service.impl
 
 import org.springframework.stereotype.Service
+import reactor.core.publisher.FluxSink
 import reactor.core.publisher.Mono
 import vottega.sse_server.adapt.KafkaProducer
+import vottega.sse_server.repository.FluxSinkRepository
 import vottega.sse_server.service.SseService
 import java.util.UUID
 
 @Service
-class SseServiceImpl(private val kafkaProducer : KafkaProducer) : SseService {
-    override fun enterRoom(roomId: Long, userId: UUID) {
+class SseServiceImpl(private val kafkaProducer: KafkaProducer, private val fluxSinkRepository: FluxSinkRepository) :
+    SseService {
+    override fun enterRoom(roomId: Long, userId: UUID, sink: FluxSink<String>) {
         kafkaProducer.participantEnterProducer(roomId, userId)
+        fluxSinkRepository.addClient(roomId, sink)
     }
 
-    override fun exitRoom(roomId: Long, userId: UUID) {
+    override fun exitRoom(roomId: Long, userId: UUID, sink: FluxSink<String>) {
         kafkaProducer.participantExitProducer(roomId, userId)
+        fluxSinkRepository.removeClient(roomId, sink)
+    }
+
+    override fun broadcastToRoom(roomId: Long, message: String) {
+        val clients = fluxSinkRepository.getClients(roomId)
+        clients.forEach { it.next(message) }
     }
 }
