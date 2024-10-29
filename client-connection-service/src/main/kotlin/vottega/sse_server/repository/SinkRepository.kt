9a7767f01dab6c6ab20b5d5b735012ffca5d3ebf
Minkyun Yun <80194344+min-guni@users.noreply.github.com>
2024-10-30 00:11:57 +0900

    fix : String -> RoomEvent 객체 emit으로 변경

--- client-connection-service/src/main/kotlin/vottega/sse_server/repository/SinkRepository.kt
+++ client-connection-service/src/main/kotlin/vottega/sse_server/repository/SinkRepository.kt
@@ -3,17 +3,18 @@ package vottega.sse_server.repository
 import org.springframework.stereotype.Repository
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Sinks
+import vottega.sse_server.dto.RoomEvent
 import java.util.concurrent.ConcurrentHashMap
 
 @Repository
 class SinkRepository {
-    private val sinkMap = ConcurrentHashMap<Long, Sinks.Many<String>>()
+    private val sinkMap = ConcurrentHashMap<Long, Sinks.Many<RoomEvent>>()
 
     fun removeRoomSink(roomId: Long) {
-        sinkMap.remove(roomId);
+        sinkMap.remove(roomId); // TODO 아직 연결되어 있는 사람이 있으면 REMOVE 안하기
     }
 
-    fun getRoomSink(roomId: Long): Flux<String> {
+    fun getRoomSink(roomId: Long): Flux<RoomEvent> {
         return sinkMap.computeIfAbsent(roomId) {
             Sinks.many().replay().latest()
         }.asFlux()
