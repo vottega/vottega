eec55cd746df7e87e4d355e557b977176cf39962
Minkyun Yun <80194344+min-guni@users.noreply.github.com>
2024-10-30 00:13:22 +0900

    feature : sse 연결 최적화 및 연결 끊길 시에 로직 추가

--- client-connection-service/src/main/kotlin/vottega/sse_server/web/SseController.kt
+++ client-connection-service/src/main/kotlin/vottega/sse_server/web/SseController.kt
@@ -3,27 +3,20 @@ package vottega.sse_server.web
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Flux
-import vottega.sse_server.adapt.KafkaConsumer
+import vottega.sse_server.dto.RoomEvent
 import vottega.sse_server.service.SseService
-import java.time.LocalTime
 import java.util.UUID
-import kotlin.time.Duration
 
 
 @RestController
 class SseController(private val sseService: SseService) {
 
     @GetMapping("/sse/room/{roomId}", produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
-    fun streamKafkaMessages(@PathVariable roomId: Long, userId: UUID): Flux<String> { // TODO Argument Resolver로 변경
-
-        return Flux.create<String> { sink ->
-            sseService.enterRoom(roomId, userId, sink)
-            sink.onCancel {
-                sseService.exitRoom(roomId, userId, sink)
-            }
+    fun connectToRoom(@PathVariable roomId: Long, userId : UUID): Flux<RoomEvent> {
+        return sseService.enterRoom(roomId, userId).doOnCancel {
+            sseService.exitRoom(roomId, userId)
         }
     }
 }
