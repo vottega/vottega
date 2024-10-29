56595b4ba000c9d4b51bd65ea9ca046ddd9705a0
Minkyun Yun <80194344+min-guni@users.noreply.github.com>
2024-10-30 00:46:54 +0900

    feature : kafkaConsumer 개발

--- client-connection-service/src/main/kotlin/vottega/sse_server/adapt/KafkaConsumer.kt
+++ client-connection-service/src/main/kotlin/vottega/sse_server/adapt/KafkaConsumer.kt
@@ -1,24 +1,63 @@
 package vottega.sse_server.adapt
 
 import org.springframework.stereotype.Component
-import reactor.core.publisher.Flux
-import vottega.sse_server.service.SseService
+import reactor.kafka.receiver.KafkaReceiver
+import reactor.kafka.receiver.ReceiverOptions
+import vottega.sse_server.config.KafkaConfig
+import vottega.sse_server.dto.mapper.RoomEventMapper
+import vottega.sse_server.repository.SinkRepository
 
 @Component
-class KafkaConsumer(val sseService: SseService) {
-    fun shortHandConsume() {
-        return sseService.broadcastToRoom(1, "shortHandConsume")
+class KafkaConsumer(val fluxSinkRepository: SinkRepository, val roomEventMapper: RoomEventMapper, private val kafkaConfig : KafkaConfig) {
+
+
+    private val roomInfoReceiverOptions = getReceiverOptions("room-info-topic")
+    private val voteInfoReceiverOptions = getReceiverOptions("vote-info-topic")
+    private val shorthandInfoReceiverOptions = getReceiverOptions("shorthand-info-topic")
+
+    private fun getReceiverOptions(topic: String) = ReceiverOptions.create<String, String>(
+        mapOf(
+            "bootstrap.servers" to kafkaConfig.bootstrapServers,
+            "group.id" to kafkaConfig.groupId,
+            "key.deserializer" to Class.forName(kafkaConfig.keyDeserializer),
+            "value.deserializer" to Class.forName(kafkaConfig.valueDeserializer),
+            "auto.offset.reset" to kafkaConfig.autoOffsetReset
+        )
+    ).subscription(listOf(topic))
+
+
+    init {
+        consumeRoomInfoMessages()
+        consumeVoteInfoMessages()
+        consumeShorthandInfoMessages()
+    }
+    private fun consumeRoomInfoMessages() {
+        KafkaReceiver.create(roomInfoReceiverOptions)
+            .receive()
+            .doOnNext { record ->
+                val event = roomEventMapper.roomDataToRoomEvent(record.value())
+                fluxSinkRepository.getRoomSink(event.roomId).tryEmitNext(event)
+            }
+            .subscribe()
     }
 
-    fun voteInfoConsume() {
-        return sseService.broadcastToRoom(1, "voteInfoConsume")
+    private fun consumeVoteInfoMessages() {
+        KafkaReceiver.create(voteInfoReceiverOptions)
+            .receive()
+            .doOnNext { record ->
+                val event = roomEventMapper.voteDataToRoomEvent(record.value())
+                fluxSinkRepository.getRoomSink(event.roomId).tryEmitNext(event)
+            }
+            .subscribe()
     }
 
-    fun roomInfoConsume() {
-        return sseService.broadcastToRoom(1, "roomInfoConsume")
-    }
-
-    private fun pasreMessage(){
-        //TODO
+    private fun consumeShorthandInfoMessages() {
+        KafkaReceiver.create(shorthandInfoReceiverOptions)
+            .receive()
+            .doOnNext { record ->
+                val event = roomEventMapper.shorthandDataToRoomEvent(record.value())
+                fluxSinkRepository.getRoomSink(event.roomId).tryEmitNext(event)
+            }
+            .subscribe()
     }
 }
