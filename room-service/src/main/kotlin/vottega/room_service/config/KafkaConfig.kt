b64a5eaed1d5d7c1c374e697f1254f884635677e
Minkyun Yun <80194344+min-guni@users.noreply.github.com>
2024-11-17 15:06:45 +0900

    fix : 기존 Config 파일 삭제

--- room-service/src/main/kotlin/vottega/room_service/config/KafkaConfig.kt
+++
@@ -1,9 +0,0 @@
-package vottega.room_service.config
-
-import org.springframework.context.annotation.Configuration
-import org.springframework.kafka.annotation.EnableKafka
-
-@EnableKafka
-@Configuration
-class KafkaConfig {
-}
