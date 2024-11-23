4e02b3ab1702e8bb964ad6b12b6052cd0ce07257
Minkyun Yun <80194344+min-guni@users.noreply.github.com>
2024-11-23 23:15:38 +0900

    feature : kafka consumer 작성

--- room-service/src/main/kotlin/vottega/room_service/adaptor/RoomConsumer.kt
+++ room-service/src/main/kotlin/vottega/room_service/adaptor/RoomConsumer.kt
@@ -2,16 +2,20 @@ package vottega.room_service.adaptor
 
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Service
+import vottega.room_service.domain.Participant
 import vottega.room_service.service.RoomService
 
 @Service
 class RoomConsumer(val roomService: RoomService
 ) {
-    @KafkaListener(topics = ["room"])
-    fun participantConnectEventConsume(message: String) {
-        //TODO 사람 입장 및 퇴장 이벤트 처리 (From Connection Service)
-        roomService.exitParticipant(message)
-        roomService.enterParticipant(message)
+    @KafkaListener(topics = ["participant-out"], groupId = "room-service", containerFactory = 'participantKafkaListenerContainerFactory') // todo 변수 다른데 저장
+    fun participantConnectEventConsume(data: ParticipantConnectData) {
+        if(data.action == 'ENTER'){ // TODO 개발하기
+            roomService.participantEnter(data.roomId, data.participantId)
+        } else {
+            roomService.participantExit(data.roomId, data.participantId)
+        }
         //카프카 좀 더 배워서 실행
     }
+
 }
