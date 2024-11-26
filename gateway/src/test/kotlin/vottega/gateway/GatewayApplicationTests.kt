eeeac61b374ea21fd0f07aa63ac2ecca84e89e2d
Minkyun Yun <80194344+min-guni@users.noreply.github.com>
2024-11-26 23:12:43 +0900

    feature : spring init

---
+++ gateway/src/test/kotlin/vottega/gateway/GatewayApplicationTests.kt
@@ -0,0 +1,13 @@
+package vottega.gateway
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
