7e96378bd66c51c5ed6f9ef8fd4e8aa51e075b6f
minguni <80194344+min-guni@users.noreply.github.com>
2024-12-07 23:43:29 +0900

    feature : feign client 작성

---
+++ auth-service/src/main/kotlin/vottega/auth_server/client/RoomClient.kt
@@ -0,0 +1,18 @@
+package vottega.auth_server.client
+
+import org.springframework.cloud.openfeign.FeignClient
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import reactor.core.publisher.Mono
+import java.util.*
+
+@FeignClient(name = "room-service", url = "http://localhost:8082", configuration = [FeignWebFluxConfig::class])
+interface RoomClient {
+    @GetMapping("/users/{id}")
+    fun getUserById(@PathVariable("id") id: UUID): Mono<UserResponse>
+}
+
+data class UserResponse(
+    val id: UUID,
+    val roomId : Long,
+)
