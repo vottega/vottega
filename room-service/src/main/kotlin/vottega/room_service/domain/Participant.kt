6bbb6fe8dd34a9cbcb677503c4a07bba4516fb27
minguni <80194344+min-guni@users.noreply.github.com>
2025-01-04 01:25:00 +0900

    fix : nullable 수정

--- room-service/src/main/kotlin/vottega/room_service/domain/Participant.kt
+++ room-service/src/main/kotlin/vottega/room_service/domain/Participant.kt
@@ -1,68 +1,65 @@
 package vottega.room_service.domain
 
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
-import jakarta.persistence.JoinColumn
-import jakarta.persistence.ManyToOne
-import jakarta.persistence.PrePersist
-import jakarta.persistence.PreUpdate
-import org.hibernate.annotations.Where
+import jakarta.persistence.*
 import java.time.LocalDateTime
 import java.util.*
 
 @Entity
 data class Participant(
-    var name: String,
-    var phoneNumber: String? = null,
-    var position: String? = null,
-    @ManyToOne
-    @JoinColumn(name = "qualification_id", nullable = false)
-    var participantRole: ParticipantRole? = null,
-    var isEntered: Boolean = false,
-    @ManyToOne
-    @JoinColumn(name = "room_id", nullable = false)
-    val room: Room,
+  var name: String,
+  var phoneNumber: String? = null,
+  var position: String? = null,
+  @ManyToOne
+  @JoinColumn(name = "qualification_id", nullable = false)
+  var participantRole: ParticipantRole,
+  var isEntered: Boolean = false,
+  @ManyToOne
+  @JoinColumn(name = "room_id", nullable = false)
+  val room: Room,
 
-    ) {
+  ) {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    val id: UUID? = null
-    var enteredAt: LocalDateTime? = null
-    var createdAt: LocalDateTime? = null
-    var lastUpdatedAt: LocalDateTime? = null
-    var deletedAt: LocalDateTime? = null
-    fun enter() {
-        this.isEntered = true
-        this.enteredAt = LocalDateTime.now()
-    }
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  val id: UUID? = null
+  var enteredAt: LocalDateTime? = null
+  var createdAt: LocalDateTime? = null
+  var lastUpdatedAt: LocalDateTime? = null
+  var deletedAt: LocalDateTime? = null
+  fun enter() {
+    this.isEntered = true
+    this.enteredAt = LocalDateTime.now()
+  }
 
-    fun exit() {
-        this.isEntered = false
-        this.enteredAt = LocalDateTime.now()
-    }
+  fun exit() {
+    this.isEntered = false
+    this.enteredAt = LocalDateTime.now()
+  }
 
-    fun remove() {
-        this.deletedAt = LocalDateTime.now()
-    }
+  fun remove() {
+    this.deletedAt = LocalDateTime.now()
+  }
 
-    fun updateParticipant(name: String? = null, phoneNumber: String? ,position: String? = null, participantRole: ParticipantRole? = null) {
-        name?.let { this.name = it }
-        phoneNumber?.let { this.phoneNumber = it }
-        position?.let { this.position = it }
-        participantRole?.let { this.participantRole = it }
-    }
+  fun updateParticipant(
+    name: String? = null,
+    phoneNumber: String?,
+    position: String? = null,
+    participantRole: ParticipantRole? = null
+  ) {
+    name?.let { this.name = it }
+    phoneNumber?.let { this.phoneNumber = it }
+    position?.let { this.position = it }
+    participantRole?.let { this.participantRole = it }
+  }
 
-    @PrePersist
-    fun prePersist() {
-        this.createdAt = LocalDateTime.now()
-        this.lastUpdatedAt = LocalDateTime.now()
-    }
+  @PrePersist
+  fun prePersist() {
+    this.createdAt = LocalDateTime.now()
+    this.lastUpdatedAt = LocalDateTime.now()
+  }
 
-    @PreUpdate
-    fun preUpdate() {
-        this.lastUpdatedAt = LocalDateTime.now()
-    }
+  @PreUpdate
+  fun preUpdate() {
+    this.lastUpdatedAt = LocalDateTime.now()
+  }
 }
--- room-service/src/main/kotlin/vottega/room_service/domain/ParticipantRole.kt
+++ room-service/src/main/kotlin/vottega/room_service/domain/ParticipantRole.kt
@@ -6,18 +6,19 @@ import jakarta.persistence.*
 class ParticipantRole(
     room: Room,
     role: String,
-    canVote: Boolean?
+    canVote: Boolean = true
 ) {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null
 
-    @ManyToOne @JoinColumn(name = "room_id", nullable = false)
+    @ManyToOne
+    @JoinColumn(name = "room_id", nullable = false)
     var room: Room = room
         private set
     var role: String = role
         private set
-    var canVote: Boolean? = canVote
+    var canVote: Boolean = canVote
         private set
 
     fun updateCanVote(canVote: Boolean) {
