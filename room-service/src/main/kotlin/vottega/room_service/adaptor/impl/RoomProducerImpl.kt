0a453f6c9ccd5550f06c3b8046ceda42bb79fc7f
minguni <80194344+min-guni@users.noreply.github.com>
2025-01-04 15:47:44 +0900

    feature : Kafka Producer Impl 구현

--- room-service/src/main/kotlin/vottega/room_service/adaptor/impl/RoomProducerImpl.kt
+++ room-service/src/main/kotlin/vottega/room_service/adaptor/impl/RoomProducerImpl.kt
@@ -1,34 +1,29 @@
 package vottega.room_service.adaptor.impl
 
+import main.room_service.avro.RoomAvro
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Component
 import vottega.room_service.adaptor.RoomProducer
-import vottega.room_service.domain.Participant
-import vottega.room_service.domain.Room
+import vottega.room_service.avro.ParticipantAvro
+import vottega.room_service.dto.ParticipantResponseDTO
+import vottega.room_service.dto.RoomResponseDTO
 import vottega.room_service.dto.mapper.ParticipantMapper
 import vottega.room_service.dto.mapper.RoomMapper
 
 @Component
 class RoomProducerImpl(
-  private val kafkaTemplate: KafkaTemplate<String, ByteArray>,
+  private val roomKafkaTemplate: KafkaTemplate<Long, RoomAvro>,
+  private val participantKafkaTemplate: KafkaTemplate<Long, ParticipantAvro>,
   private val roomMapper: RoomMapper,
   private val participantMapper: ParticipantMapper,
 ) : RoomProducer {
-  override fun roomEditMessageProduce(room: Room) {
-    //TODO 방 수정 이벤트 발생
-    val roomOutDTO = roomMapper.toRoomOutDTO(room)
-    //TODO avro로 변환
-
-    //TODO kafka로 전송
-    kafkaTemplate.send("room", roomOutDTO.id.toString(), roomOutDTO.toString().toByteArray())
+  override fun roomEditMessageProduce(roomResponseDTO: RoomResponseDTO) {
+    val roomOutAvro = roomMapper.toRoomAvro(roomResponseDTO)
+    roomKafkaTemplate.send("room", roomOutAvro.id, roomOutAvro)
   }
 
-  override fun participantEditMessageProduce(participant: Participant) {
-    //TODO 방에 있는 사람 수정 이벤트 발생
-    val participantInfoDTO = participantMapper.toParticipantResponseDTO(participant)
-    //TODO avro로 변환
-
-    //TODO kafka로 전송
-    kafkaTemplate.send("participant", participantInfoDTO.roomId.toString(), participantInfoDTO.toString().toByteArray())
+  override fun participantEditMessageProduce(participantResponseDTO: ParticipantResponseDTO) {
+    val participantAvro = participantMapper.toParticipantAvro(participantResponseDTO)
+    participantKafkaTemplate.send("participant", participantAvro.roomId, participantAvro)
   }
 }
