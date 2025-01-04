65d39c354d73ff9f2305826fd83fa7f03e3f9750
minguni <80194344+min-guni@users.noreply.github.com>
2025-01-04 22:48:45 +0900

    fix : fix build.gradle.kts

--- room-service/build.gradle.kts
+++ room-service/build.gradle.kts
@@ -1,72 +1,74 @@
 plugins {
-	kotlin("jvm") version "1.9.24"
-	kotlin("plugin.spring") version "1.9.24"
-	id("org.springframework.boot") version "3.3.2"
-	id("io.spring.dependency-management") version "1.1.6"
-	id("com.github.davidmc24.gradle.plugin.avro") version "1.9.1"
-	kotlin("plugin.jpa") version "1.9.24"
+  kotlin("jvm") version "1.9.24"
+  kotlin("plugin.spring") version "1.9.24"
+  id("org.springframework.boot") version "3.3.2"
+  id("io.spring.dependency-management") version "1.1.6"
+  id("com.github.davidmc24.gradle.plugin.avro") version "1.9.1"
+  kotlin("plugin.jpa") version "1.9.24"
 }
 
 group = "vottega"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(17)
-	}
+  toolchain {
+    languageVersion = JavaLanguageVersion.of(17)
+  }
 }
 
 configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
-	}
+  compileOnly {
+    extendsFrom(configurations.annotationProcessor.get())
+  }
 }
 
 ext {
-	set("springCloudVersion", "2023.0.3")
+  set("springCloudVersion", "2023.0.3")
 }
 
 dependencyManagement {
-	imports {
-		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
-	}
+  imports {
+    mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+  }
 }
 
 
 repositories {
-	mavenCentral() // 기본 저장소
-	maven("https://packages.confluent.io/maven/") // Confluent 저장소
+  mavenCentral() // 기본 저장소
+  maven("https://packages.confluent.io/maven/") // Confluent 저장소
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-data-redis")
-	implementation("org.springframework.boot:spring-boot-starter-security")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.springframework.kafka:spring-kafka")
-	compileOnly("org.projectlombok:lombok")
-	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
-	annotationProcessor("org.projectlombok:lombok")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-	testImplementation("org.springframework.kafka:spring-kafka-test")
-	testImplementation("org.springframework.security:spring-security-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-	implementation("org.apache.avro:avro:1.11.4")
-	implementation("io.confluent:kafka-avro-serializer:7.5.0")
-	implementation("org.springdoc:springdoc-openapi-ui:1.8.0")
+  implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+  implementation("org.springframework.boot:spring-boot-starter-data-redis")
+  implementation("org.springframework.boot:spring-boot-starter-security")
+  implementation("org.springframework.boot:spring-boot-starter-web")
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+  implementation("org.jetbrains.kotlin:kotlin-reflect")
+  implementation("org.springframework.kafka:spring-kafka")
+  compileOnly("org.projectlombok:lombok")
+  runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
+  annotationProcessor("org.projectlombok:lombok")
+  testImplementation("org.springframework.boot:spring-boot-starter-test")
+  testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+  testImplementation("org.springframework.kafka:spring-kafka-test")
+  testImplementation("org.springframework.security:spring-security-test")
+  testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+  implementation("org.apache.avro:avro:1.11.4")
+  implementation("io.confluent:kafka-avro-serializer:7.5.0")
+  implementation("org.springdoc:springdoc-openapi-ui:1.8.0")
 
-	implementation("org.apache.avro:avro:1.11.0")
+  implementation("org.apache.avro:avro:1.11.0")
+  implementation("org.springframework.boot:spring-boot-starter-actuator")
+  implementation("org.springframework.boot:spring-boot-starter-validation")
 }
 
 kotlin {
-	compilerOptions {
-		freeCompilerArgs.addAll("-Xjsr305=strict")
-	}
+  compilerOptions {
+    freeCompilerArgs.addAll("-Xjsr305=strict")
+  }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+  useJUnitPlatform()
 }
