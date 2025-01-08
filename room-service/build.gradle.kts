803e26087b568ee9766acf7d89078d35934a6919
minguni <80194344+min-guni@users.noreply.github.com>
2025-01-08 23:58:51 +0900

    fix : 안쓰는거 지우기

--- room-service/build.gradle.kts
+++ room-service/build.gradle.kts
@@ -40,8 +40,6 @@ repositories {
 
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-  implementation("org.springframework.boot:spring-boot-starter-data-redis")
-  implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("org.springframework.boot:spring-boot-starter-web")
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
   implementation("org.jetbrains.kotlin:kotlin-reflect")
@@ -52,6 +50,8 @@ dependencies {
   testImplementation("org.springframework.boot:spring-boot-starter-test")
   testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
   testImplementation("org.springframework.kafka:spring-kafka-test")
+  testImplementation("org.testcontainers:kafka")
+  testImplementation("org.testcontainers:junit-jupiter")
   testImplementation("org.springframework.security:spring-security-test")
   testRuntimeOnly("org.junit.platform:junit-platform-launcher")
   implementation("org.apache.avro:avro:1.11.4")
