04fb36c671f0bf87cbd4858fbdbc3ea0cab7adee
minguni <80194344+min-guni@users.noreply.github.com>
2025-01-29 00:24:53 +0900

    feature : soft delete 추가

--- vote-service/src/main/kotlin/vottega/vote_service/domain/VotePaper.kt
+++ vote-service/src/main/kotlin/vottega/vote_service/domain/VotePaper.kt
@@ -1,11 +1,15 @@
 package vottega.vote_service.domain
 
 import jakarta.persistence.*
+import org.hibernate.annotations.SQLDelete
+import org.hibernate.annotations.SQLRestriction
 import vottega.vote_service.domain.enum.VotePaperType
 import java.time.LocalDateTime
 import java.util.UUID
 
 @Entity
+@SQLDelete(sql = "UPDATE participant SET deleted_at = CURRENT_TIMESTAMP WHERE id = ?")
+@SQLRestriction("deleted_at is NULL")
 class VotePaper(
     val userId: UUID,
     @ManyToOne @JoinColumn(name = "vote_id") val vote: Vote,
