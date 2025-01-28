6a5be4636a71f271761670f9f510435c47a7536c
minguni <80194344+min-guni@users.noreply.github.com>
2025-01-28 15:39:53 +0900

    feature : local profile 에서 swagger open

--- room-service/src/main/kotlin/vottega/room_service/config/SecurityConfig.kt
+++ room-service/src/main/kotlin/vottega/room_service/config/SecurityConfig.kt
@@ -2,16 +2,44 @@ package vottega.room_service.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import org.springframework.core.annotation.Order
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.access.ExceptionTranslationFilter
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher
+import org.springframework.security.web.util.matcher.OrRequestMatcher
 import vottega.room_service.filter.CustomHeaderAuthenticationFilter
 
 @Configuration
 class SecurityConfig {
 
   @Bean
+  @Profile("local")
+  @Order(1)
+  fun webSecurityCustomizer(http: HttpSecurity): SecurityFilterChain {
+
+    val multipleMatchers = OrRequestMatcher(
+      AntPathRequestMatcher("/v3/api-docs/**"),
+      AntPathRequestMatcher("/swagger-ui/**"),
+      AntPathRequestMatcher("/api")
+    )
+    http
+      // 이 체인은 /api/** 로 들어오는 요청만 매칭
+      .securityMatcher(multipleMatchers)
+      .csrf { it.disable() }
+      .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+      .authorizeHttpRequests {
+        // 여긴 전부 허용
+        it.anyRequest().permitAll()
+      }
+    // CustomHeaderAuthenticationFilter 등록하지 않음
+    return http.build()
+  }
+
+  @Bean
+  @Order(2)
   fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
     http
       .csrf { it.disable() }
