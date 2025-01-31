3314357199f73400415d2f8418ba9e4c2b086cbd
minguni <80194344+min-guni@users.noreply.github.com>
2025-02-01 02:24:56 +0900

    fix :tab 4 -> 2

--- vote-service/src/main/kotlin/vottega/vote_service/dto/mapper/VoteDetailResponseDTOMapper.kt
+++ vote-service/src/main/kotlin/vottega/vote_service/dto/mapper/VoteDetailResponseDTOMapper.kt
@@ -1,82 +1,80 @@
 package vottega.vote_service.dto.mapper
 
 import org.springframework.stereotype.Service
-import vottega.vote_service.dto.room.RoomResponseDTO
 import vottega.vote_service.domain.Vote
 import vottega.vote_service.domain.enum.VotePaperType
-import vottega.vote_service.dto.room.ParticipantResponseDTO
+import vottega.vote_service.dto.ParticipantIdNameDTO
 import vottega.vote_service.dto.VoteDetailResponseDTO
 
 @Service
 class VoteDetailResponseDTOMapper {
-    fun toVoteDetailResponse(vote: Vote, room: RoomResponseDTO): VoteDetailResponseDTO {
-        if(vote.isSecret){
-            return VoteDetailResponseDTO(
-                id = vote.id,
-                agendaName = vote.agendaName,
-                voteName = vote.voteName,
-                status = vote.status,
-                createdAt = vote.createdAt,
-                startedAt = vote.startedAt,
-                finishedAt = vote.finishedAt,
-                passRate = vote.passRate,
-                minParticipantNumber = vote.minParticipantNumber,
-                minParticipantRate = vote.minParticipantRate,
-                isSecret = vote.isSecret,
-                result = vote.result,
-                yesList = List(vote.votePaperList.filter { it.voteResultType == VotePaperType.YES }.size) { index ->
-                    ParticipantResponseDTO(
-                        id = null,
-                        name = "anonymous user$index"
-                    )
-                },
-                noList = List(vote.votePaperList.filter { it.voteResultType == VotePaperType.NO }.size) { index ->
-                    ParticipantResponseDTO(
-                        id = null,
-                        name = "anonymous user$index"
-                    )
-                },
-                abstainList = List(vote.votePaperList.filter { it.voteResultType == VotePaperType.ABSTAIN }.size) { index ->
-                    ParticipantResponseDTO(
-                        id = null,
-                        name = "anonymous user$index"
-                    )
-                },
-            )
-        }
-
-        val participantMap = room.participants.associateBy { it.id }
-        return VoteDetailResponseDTO(
-            id = vote.id,
-            agendaName = vote.agendaName,
-            voteName = vote.voteName,
-            status = vote.status,
-            createdAt = vote.createdAt,
-            startedAt = vote.startedAt,
-            finishedAt = vote.finishedAt,
-            passRate = vote.passRate,
-            minParticipantNumber = vote.minParticipantNumber,
-            minParticipantRate = vote.minParticipantRate,
-            isSecret = vote.isSecret,
-            result = vote.result,
-            yesList = vote.votePaperList.filter { it.voteResultType == VotePaperType.YES }.map {
-                ParticipantResponseDTO(
-                    id = it.userId,
-                    name = participantMap[it.userId]?.name ?: "Unknown User"
-                )
-            },
-            noList = vote.votePaperList.filter { it.voteResultType == VotePaperType.NO }.map {
-                ParticipantResponseDTO(
-                    id = it.userId,
-                    name = participantMap[it.userId]?.name ?: "Unknown User"
-                )
-            },
-            abstainList = vote.votePaperList.filter { it.voteResultType == VotePaperType.ABSTAIN }.map {
-                ParticipantResponseDTO(
-                    id = it.userId,
-                    name = participantMap[it.userId]?.name ?: "Unknown User"
-                )
-            },
-        )
+  fun toVoteDetailResponse(vote: Vote): VoteDetailResponseDTO {
+    if (vote.isSecret) {
+      return VoteDetailResponseDTO(
+        id = vote.id,
+        agendaName = vote.agendaName,
+        voteName = vote.voteName,
+        status = vote.status,
+        createdAt = vote.createdAt,
+        startedAt = vote.startedAt,
+        finishedAt = vote.finishedAt,
+        passRate = vote.passRate,
+        minParticipantNumber = vote.minParticipantNumber,
+        minParticipantRate = vote.minParticipantRate,
+        isSecret = vote.isSecret,
+        result = vote.result,
+        yesList = List(vote.votePaperList.filter { it.voteResultType == VotePaperType.YES }.size) { index ->
+          ParticipantIdNameDTO(
+            id = null,
+            name = "anonymous user$index"
+          )
+        },
+        noList = List(vote.votePaperList.filter { it.voteResultType == VotePaperType.NO }.size) { index ->
+          ParticipantIdNameDTO(
+            id = null,
+            name = "anonymous user$index"
+          )
+        },
+        abstainList = List(vote.votePaperList.filter { it.voteResultType == VotePaperType.ABSTAIN }.size) { index ->
+          ParticipantIdNameDTO(
+            id = null,
+            name = "anonymous user$index"
+          )
+        },
+      )
     }
+
+    return VoteDetailResponseDTO(
+      id = vote.id,
+      agendaName = vote.agendaName,
+      voteName = vote.voteName,
+      status = vote.status,
+      createdAt = vote.createdAt,
+      startedAt = vote.startedAt,
+      finishedAt = vote.finishedAt,
+      passRate = vote.passRate,
+      minParticipantNumber = vote.minParticipantNumber,
+      minParticipantRate = vote.minParticipantRate,
+      isSecret = vote.isSecret,
+      result = vote.result,
+      yesList = vote.votePaperList.filter { it.voteResultType == VotePaperType.YES }.map {
+        ParticipantIdNameDTO(
+          id = it.userId,
+          name = it.userName
+        )
+      },
+      noList = vote.votePaperList.filter { it.voteResultType == VotePaperType.NO }.map {
+        ParticipantIdNameDTO(
+          id = it.userId,
+          name = it.userName
+        )
+      },
+      abstainList = vote.votePaperList.filter { it.voteResultType == VotePaperType.ABSTAIN }.map {
+        ParticipantIdNameDTO(
+          id = it.userId,
+          name = it.userName
+        )
+      },
+    )
+  }
 }
--- vote-service/src/main/kotlin/vottega/vote_service/service/VoteService.kt
+++ vote-service/src/main/kotlin/vottega/vote_service/service/VoteService.kt
@@ -1,19 +1,20 @@
 package vottega.vote_service.service
 
 import vottega.vote_service.domain.enum.VotePaperType
+import vottega.vote_service.domain.enum.VoteStatus
 import vottega.vote_service.dto.VoteDetailResponseDTO
 import vottega.vote_service.dto.VoteRequestDTO
 import vottega.vote_service.dto.VoteResponseDTO
 import java.util.*
 
 interface VoteService {
-    fun createVote(roomId: Long, voteRequestDTO: VoteRequestDTO) : VoteDetailResponseDTO
+  fun createVote(roomId: Long, voteRequestDTO: VoteRequestDTO): VoteDetailResponseDTO
 
-    fun editVote(roomId: Long, voteRequestDTO: VoteRequestDTO)
-    fun editVoteStatus(voteId: Long, action : String) : VoteDetailResponseDTO
-    fun addVotePaper(voteId: Long, userId: UUID, voteResultType: VotePaperType)
-    fun getVoteInfo(roomId: Long) : List<VoteResponseDTO>
-    fun getVoteDetail(voteId: Long) : VoteDetailResponseDTO
+  fun editVote(roomId: Long, voteRequestDTO: VoteRequestDTO)
+  fun editVoteStatus(voteId: Long, action: VoteStatus): VoteDetailResponseDTO
+  fun addVotePaper(voteId: Long, userId: UUID, voteResultType: VotePaperType)
+  fun getVoteInfo(roomId: Long): List<VoteResponseDTO>
+  fun getVoteDetail(voteId: Long): VoteDetailResponseDTO
 
-    fun resetVote(voteId: Long)
+  fun resetVote(voteId: Long)
 }
