f10a971907d0433a0e2b1e9a54b170e8146cc12d
minguni <80194344+min-guni@users.noreply.github.com>
2025-02-02 02:56:27 +0900

    fix : change variable name

--- vote-service/src/main/kotlin/vottega/vote_service/domain/Vote.kt
+++ vote-service/src/main/kotlin/vottega/vote_service/domain/Vote.kt
@@ -1,9 +1,9 @@
 package vottega.vote_service.domain
 
 import jakarta.persistence.*
+import vottega.vote_service.domain.enum.Status
 import vottega.vote_service.domain.enum.VotePaperType
-import vottega.vote_service.domain.enum.VoteResultType
-import vottega.vote_service.domain.enum.VoteStatus
+import vottega.vote_service.domain.enum.VoteResult
 import vottega.vote_service.dto.room.RoomResponseDTO
 import vottega.vote_service.exception.VoteStatusConflictException
 import java.time.LocalDateTime
@@ -47,13 +47,13 @@ class Vote(
     private set
   var finishedAt: LocalDateTime? = null
     private set
-  var status: VoteStatus = VoteStatus.CREATED
+  var status: Status = Status.CREATED
     private set
 
   var passRate: FractionVO = passRate ?: FractionVO(1, 2)
     private set
 
-  var result: VoteResultType = VoteResultType.NOT_DECIDED
+  var result: VoteResult = VoteResult.NOT_DECIDED
     private set
 
   fun update(
@@ -65,7 +65,7 @@ class Vote(
     minParticipantNumber: Int?,
     minParticipantRate: FractionVO?
   ) {
-    if (status != VoteStatus.CREATED) {
+    if (status != Status.CREATED) {
       throw VoteStatusConflictException("투표가 진행 중이라 수정할 수 없습니다.")
     }
     agendaName?.let { this.agendaName = it }
@@ -78,14 +78,14 @@ class Vote(
   }
 
   fun startVote(room: RoomResponseDTO) {
-    if (status == VoteStatus.CREATED) {
+    if (status == Status.CREATED) {
       room.participants.filter { it.participantRole.canVote }.forEach {
         votePaperList.add(VotePaper(it.id, this, it.name))
       }
       if (votePaperList.size < minParticipantNumber && votePaperList.size < minParticipantRate.multipy(room.participants.size)) {
         throw VoteStatusConflictException("참여자 수가 부족합니다.")
       }
-      status = VoteStatus.STARTED
+      status = Status.STARTED
       startedAt = LocalDateTime.now()
     } else {
       throw VoteStatusConflictException("이미 시작된 투표입니다.")
@@ -93,14 +93,14 @@ class Vote(
   }
 
   fun endVote() {
-    if (status == VoteStatus.STARTED) {
-      status = VoteStatus.ENDED
+    if (status == Status.STARTED) {
+      status = Status.ENDED
       finishedAt = LocalDateTime.now()
-      val yesNum = votePaperList.count { it.voteResultType == VotePaperType.YES }
+      val yesNum = votePaperList.count { it.votePaperType == VotePaperType.YES }
       if (yesNum >= passRate.multipy(votePaperList.size)) {
-        result = VoteResultType.PASSED
+        result = VoteResult.PASSED
       } else {
-        result = VoteResultType.REJECTED
+        result = VoteResult.REJECTED
       }
     } else {
       throw VoteStatusConflictException("아직 시작되지 않은 투표입니다.")
@@ -108,21 +108,21 @@ class Vote(
   }
 
   fun resetVote() {
-    if (status == VoteStatus.STARTED) {
+    if (status == Status.STARTED) {
       votePaperList.clear()
-      status = VoteStatus.CREATED
+      status = Status.CREATED
     } else {
       throw VoteStatusConflictException("투표가 진행 중이지 않습니다.")
     }
   }
 
   fun addVotePaper(userId: UUID, voteResultType: VotePaperType) {
-    if (status != VoteStatus.STARTED) {
+    if (status != Status.STARTED) {
       throw VoteStatusConflictException("투표가 시작되지 않았습니다.")
     }
     votePaperList.forEach {
       //이미 투표한 사람인지 확인
-      if (it.userId == userId && it.voteResultType != VotePaperType.NOT_VOTED) {
+      if (it.userId == userId && it.votePaperType != VotePaperType.NOT_VOTED) {
         throw VoteStatusConflictException("이미 투표한 사람입니다.")
       } else if (it.userId == userId) {
         it.vote(voteResultType)
