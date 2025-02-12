e6036abdb718f7f1b2a7e1876afd66d5d27ad355
minguni <80194344+min-guni@users.noreply.github.com>
2025-02-12 19:19:08 +0900

    fix : 분모에 0 못하게 설정

--- vote-service/src/main/kotlin/vottega/vote_service/domain/FractionVO.kt
+++ vote-service/src/main/kotlin/vottega/vote_service/domain/FractionVO.kt
@@ -3,8 +3,12 @@ package vottega.vote_service.domain
 import jakarta.persistence.Embeddable
 
 @Embeddable
-data class FractionVO(val numerator : Int, val denominator: Int) {
-    fun multipy(number : Int): Double {
-        return (number * numerator).toDouble() / denominator.toDouble()
-    }
+data class FractionVO(val numerator: Int, val denominator: Int) {
+  init {
+    require(denominator != 0) { "Denominator cannot be zero" }
+  }
+
+  fun multiply(number: Int): Double {
+    return (number * numerator).toDouble() / denominator.toDouble()
+  }
 }
