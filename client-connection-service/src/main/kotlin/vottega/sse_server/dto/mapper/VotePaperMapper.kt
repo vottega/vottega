8e7b839a96f9bc237f19e96b6e6d7c9017e26726
minguni <80194344+min-guni@users.noreply.github.com>
2025-02-23 12:27:18 +0900

    fix : votepaper avro 수정

--- client-connection-service/src/main/kotlin/vottega/sse_server/dto/mapper/VotePaperMapper.kt
+++ client-connection-service/src/main/kotlin/vottega/sse_server/dto/mapper/VotePaperMapper.kt
@@ -8,37 +8,25 @@ import java.time.ZoneId
 //TODO 이거 해야됨
 @Component
 class VotePaperMapper {
-  fun toVotePaperDTO(votePaper: VotePaper): VotePaperDTO {
+  fun toVotePaperDTO(votePaperAvro: VotePaperAvro): VotePaperDTO {
     return VotePaperDTO(
-      votePaperId = votePaper.id ?: throw IllegalStateException("VotePaper's id is null"),
-      voteId = votePaper.vote.id ?: throw IllegalStateException("VotePaper's vote.id is null"),
-      roomId = votePaper.vote.roomId,
-      userId = votePaper.userId,
-      userName = votePaper.userName,
-      votePaperType = votePaper.votePaperType,
-      createdAt = votePaper.createdAt ?: throw IllegalStateException("VotePaper's createdAt is null"),
-      votedAt = votePaper.votedAt,
+      votePaperId = votePaperAvro.id,
+      roomId = votePaperAvro.roomId,
+      voteId = votePaperAvro.voteId,
+      userId = votePaperAvro.userId,
+      userName = votePaperAvro.userName,
+      votePaperType = avroEnumToVotePaperType(votePaperAvro.votePaperType),
+      createdAt = votePaperAvro.createdAt.atZone(ZoneId.systemDefault()).toLocalDateTime(),
+      votedAt = votePaperAvro.votedAt?.atZone(ZoneId.systemDefault())?.toLocalDateTime()
     )
   }
 
-  fun toVotePaperAvro(votePaperDTO: VotePaperDTO): VotePaperAvro {
-    return VotePaperAvro.newBuilder()
-      .setId(votePaperDTO.votePaperId)
-      .setVoteId(votePaperDTO.voteId)
-      .setUserId(votePaperDTO.userId)
-      .setUserName(votePaperDTO.userName)
-      .setVotePaperType(votePaperTypeToAvroEnum(votePaperDTO.votePaperType))
-      .setCreatedAt(votePaperDTO.createdAt.atZone(ZoneId.systemDefault()).toInstant())
-      .setVotedAt(votePaperDTO.votedAt?.atZone(ZoneId.systemDefault())?.toInstant())
-      .build()
-  }
-
-  fun votePaperTypeToAvroEnum(votePaperType: VotePaperType): vottega.vote_service.avro.VotePaperType {
+  fun avroEnumToVotePaperType(votePaperType: vottega.sse_server.avro.VotePaperType): VotePaperType {
     return when (votePaperType) {
-      VotePaperType.YES -> vottega.vote_service.avro.VotePaperType.YES
-      VotePaperType.NO -> vottega.vote_service.avro.VotePaperType.NO
-      VotePaperType.ABSTAIN -> vottega.vote_service.avro.VotePaperType.ABSTAIN
-      VotePaperType.NOT_VOTED -> vottega.vote_service.avro.VotePaperType.NOT_VOTED
+      vottega.sse_server.avro.VotePaperType.YES -> VotePaperType.YES
+      vottega.sse_server.avro.VotePaperType.NO -> VotePaperType.NO
+      vottega.sse_server.avro.VotePaperType.ABSTAIN -> VotePaperType.ABSTAIN
+      vottega.sse_server.avro.VotePaperType.NOT_VOTED -> VotePaperType.NOT_VOTED
     }
   }
 }
