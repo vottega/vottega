799ccebd294248c5796c015440dba9cfb0747caa
minguni <80194344+min-guni@users.noreply.github.com>
2025-02-24 22:35:50 +0900

    fix : impl 구조 바꾸기

--- room-service/src/main/kotlin/vottega/room_service/adaptor/RoomProducer.kt
+++ room-service/src/main/kotlin/vottega/room_service/adaptor/RoomProducer.kt
@@ -1,15 +1,30 @@
 package vottega.room_service.adaptor
 
+import main.room_service.avro.RoomAvro
+import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Component
 import vottega.room_service.avro.Action
+import vottega.room_service.avro.ParticipantAvro
 import vottega.room_service.dto.ParticipantResponseDTO
 import vottega.room_service.dto.RoomResponseDTO
+import vottega.room_service.dto.mapper.ParticipantMapper
+import vottega.room_service.dto.mapper.RoomMapper
 
 
 @Component
-interface RoomProducer {
+class RoomProducer(
+  private val roomKafkaTemplate: KafkaTemplate<Long, RoomAvro>,
+  private val participantKafkaTemplate: KafkaTemplate<Long, ParticipantAvro>,
+  private val roomMapper: RoomMapper,
+  private val participantMapper: ParticipantMapper,
+) {
+  fun roomEditMessageProduce(roomResponseDTO: RoomResponseDTO) {
+    val roomOutAvro = roomMapper.toRoomAvro(roomResponseDTO)
+    roomKafkaTemplate.send("room", roomOutAvro.id, roomOutAvro)
+  }
 
-  fun roomEditMessageProduce(roomResponseDTO: RoomResponseDTO)
-  fun participantEditMessageProduce(participantResponseDTO: ParticipantResponseDTO, action: Action)
-
+  fun participantEditMessageProduce(participantResponseDTO: ParticipantResponseDTO, action: Action) {
+    val participantAvro = participantMapper.toParticipantAvro(participantResponseDTO, action)
+    participantKafkaTemplate.send("participant", participantAvro.roomId, participantAvro)
+  }
 }
--- room-service/src/main/kotlin/vottega/room_service/service/RoomService.kt
+++ room-service/src/main/kotlin/vottega/room_service/service/RoomService.kt
@@ -1,32 +1,144 @@
-package vottega.room_service.service
+package vottega.room_service.service.impl
 
+import jakarta.transaction.Transactional
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.stereotype.Service
+import vottega.room_service.adaptor.RoomProducer
+import vottega.room_service.avro.Action
+import vottega.room_service.domain.Room
 import vottega.room_service.domain.enumeration.RoomStatus
 import vottega.room_service.dto.ParticipantInfoDTO
 import vottega.room_service.dto.ParticipantRoleDTO
+import vottega.room_service.dto.ParticipantRoomDTO
 import vottega.room_service.dto.RoomResponseDTO
+import vottega.room_service.dto.mapper.ParticipantMapper
+import vottega.room_service.dto.mapper.RoomMapper
+import vottega.room_service.exception.ParticipantNotFoundException
+import vottega.room_service.exception.RoomNotFoundException
+import vottega.room_service.repository.ParticipantRepository
+import vottega.room_service.repository.RoomRepository
 import java.util.*
 
-interface RoomService {
+@Service
+@Transactional
+class RoomService(
+  private val roomRepository: RoomRepository,
+  private val participantRepository: ParticipantRepository,
+  private val roomMapper: RoomMapper,
+  private val roomProducer: RoomProducer,
+  private val participantMapper: ParticipantMapper
+) {
 
+  @PreAuthorize("hasRole('USER')") //  todo ownerid를 받지 말고 token parsing해서 받도록
   fun createRoom(
     roomName: String,
     ownerId: Long,
     participantRoleDTOList: List<ParticipantRoleDTO>,
-  ): RoomResponseDTO
+  ): RoomResponseDTO {
+    val room = Room(roomName, ownerId)
+    roomRepository.save(room)
+    participantRoleDTOList.forEach { room.addParticipantRole(it.role, it.canVote) }
 
-  fun updateRoom(roomId: Long, roomName: String? = null, status: RoomStatus? = null): RoomResponseDTO
-  fun addParticipant(roomId: Long, participantInfoDTOS: List<ParticipantInfoDTO>): RoomResponseDTO
-  fun removeParticipant(roomId: Long, participantId: UUID): RoomResponseDTO
-  fun updateParticipant(roomId: Long, participantId: UUID, participantInfoDTO: ParticipantInfoDTO): RoomResponseDTO
-  fun enterParticipant(roomId: Long, participantId: UUID): RoomResponseDTO
-  fun exitParticipant(roomId: Long, participantId: UUID): RoomResponseDTO
+    return roomMapper.toRoomOutDTO(room)
+  }
 
-  fun addRole(roomId: Long, roleInfo: ParticipantRoleDTO): RoomResponseDTO
 
-  fun deleteRole(roomId: Long, role: String): RoomResponseDTO
+  @PreAuthorize("@roomSecurity.isOwner(#roomId, authentication.principal)")
+  fun updateRoom(roomId: Long, roomName: String?, status: RoomStatus?): RoomResponseDTO {
+    val room = roomRepository.findById(roomId).orElseThrow { RoomNotFoundException(roomId) }
+    room.update(roomName, status)
+    val roomDTO = roomMapper.toRoomOutDTO(room)
+    roomProducer.roomEditMessageProduce(roomDTO)
+    return roomDTO
+  }
 
-  fun getRoom(roomId: Long): RoomResponseDTO
 
-  fun getRoomList(userId: Long): List<RoomResponseDTO>
+  @PreAuthorize("@roomSecurity.isOwner(#roomId, authentication.principal)")
+  fun addParticipant(roomId: Long, participantInfoDTOS: List<ParticipantInfoDTO>): RoomResponseDTO {
+    val room = roomRepository.findById(roomId).orElseThrow { RoomNotFoundException(roomId) }
+    participantInfoDTOS.forEach {
+      room.addParticipant(it)
+    }
+    roomRepository.save(room)
+    participantInfoDTOS.forEach { participantInfoDTO ->
+      room.participantList.find { participantInfoDTO.name == it.name }?.let { participant ->
+        roomProducer.participantEditMessageProduce(participantMapper.toParticipantResponseDTO(participant), Action.ADD)
+      }
+    }
+    return roomMapper.toRoomOutDTO(room)
+  }
 
+
+  @PreAuthorize("@roomSecurity.isOwner(#roomId, authentication.principal)")
+  fun removeParticipant(roomId: Long, participantId: UUID): RoomResponseDTO {
+    //TODO 방장은 못지우게
+    val room = roomRepository.findById(roomId).orElseThrow { RoomNotFoundException(roomId) }
+    val removedParticipant = room.removeParticipant(participantId)
+    roomRepository.save(room)
+    roomProducer.participantEditMessageProduce(
+      participantMapper.toParticipantResponseDTO(removedParticipant),
+      Action.DELETE
+    )
+    return roomMapper.toRoomOutDTO(room)
+  }
+
+
+  @PreAuthorize("@roomSecurity.isOwner(#roomId, authentication.principal)")
+  fun updateParticipant(
+    roomId: Long,
+    participantId: UUID,
+    participantInfoDTO: ParticipantInfoDTO
+  ): RoomResponseDTO {
+    val room = roomRepository.findById(roomId).orElseThrow { RoomNotFoundException(roomId) }
+    val updatedParticipant = room.updateParticipant(participantId, participantInfoDTO)
+    val participantDTO = participantMapper.toParticipantResponseDTO(updatedParticipant)
+    roomProducer.participantEditMessageProduce(participantDTO, Action.EDIT)
+    return roomMapper.toRoomOutDTO(room)
+  }
+
+
+  fun enterParticipant(roomId: Long, participantId: UUID): RoomResponseDTO {
+    val room = roomRepository.findById(roomId).orElseThrow { RoomNotFoundException(roomId) }
+    room.apply { enterParticipant(participantId) }
+    return roomMapper.toRoomOutDTO(room)
+  }
+
+  fun exitParticipant(roomId: Long, participantId: UUID): RoomResponseDTO {
+    val room = roomRepository.findById(roomId).orElseThrow { RoomNotFoundException(roomId) }
+    room.apply { exitParticipant(participantId) }
+    return roomMapper.toRoomOutDTO(room)
+  }
+
+
+  @PreAuthorize("@roomSecurity.isOwner(#roomId, authentication.principal)")
+  fun addRole(roomId: Long, roleInfo: ParticipantRoleDTO): RoomResponseDTO {
+    val room = roomRepository.findById(roomId).orElseThrow { RoomNotFoundException(roomId) }
+    room.apply { addParticipantRole(roleInfo.role, roleInfo.canVote) }
+    return roomMapper.toRoomOutDTO(room)
+  }
+
+
+  @PreAuthorize("@roomSecurity.isOwner(#roomId, authentication.principal)")
+  fun deleteRole(roomId: Long, role: String): RoomResponseDTO {
+    val room = roomRepository.findById(roomId).orElseThrow { RoomNotFoundException(roomId) }
+    room.apply { deleteParticipantRole(role) }
+    return roomMapper.toRoomOutDTO(room)
+  }
+
+  @PreAuthorize("@roomSecurity.isParticipantInRoom(#roomId, authentication.principal)")
+  fun getRoom(roomId: Long): RoomResponseDTO {
+    val room = roomRepository.findById(roomId).orElseThrow { RoomNotFoundException(roomId) }
+    return roomMapper.toRoomOutDTO(room)
+  }
+
+  fun getRoomList(userId: Long): List<RoomResponseDTO> {
+    return roomRepository.findByUserId(userId).map { roomMapper.toRoomOutDTO(it) }
+  }
+
+  fun getParticipantRoom(participantId: UUID): ParticipantRoomDTO {
+    return participantRepository.findById(participantId).orElseThrow { ParticipantNotFoundException(participantId) }
+      .let {
+        ParticipantRoomDTO(it.room.id ?: throw IllegalStateException("Room Id is null"))
+      }
+  }
 }
