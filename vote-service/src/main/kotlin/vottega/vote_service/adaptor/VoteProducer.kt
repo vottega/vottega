81d8700b9db8375def43b028b94e832db0d1801b
minguni <80194344+min-guni@users.noreply.github.com>
2025-02-24 22:36:41 +0900

    fix : impl 구조 바꾸기

--- vote-service/src/main/kotlin/vottega/vote_service/adaptor/VoteProducer.kt
+++ vote-service/src/main/kotlin/vottega/vote_service/adaptor/VoteProducer.kt
@@ -1,11 +1,32 @@
-package vottega.vote_service.adaptor
+package vottega.vote_service.adaptor.impl
 
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.stereotype.Component
 import vottega.vote_service.avro.VoteAction
+import vottega.vote_service.avro.VoteAvro
+import vottega.vote_service.avro.VotePaperAvro
 import vottega.vote_service.dto.VotePaperDTO
 import vottega.vote_service.dto.VoteResponseDTO
+import vottega.vote_service.dto.mapper.VotePaperMapper
+import vottega.vote_service.dto.mapper.VoteResponseDTOMapper
 
-interface VoteProducer {
-  fun voteUpdatedMessageProduce(voteResponseDTO: VoteResponseDTO, action: VoteAction)
-  fun votePaperAddedMessageProduce(votePaperDTO: VotePaperDTO)
+@Component
+class VoteProducer(
+  private val voteKafkaTemplate: KafkaTemplate<Long, VoteAvro>,
+  private val votePaperKafkaTemplate: KafkaTemplate<Long, VotePaperAvro>,
+  private val voteResponseDTOMapper: VoteResponseDTOMapper,
+  private val votePaperMapper: VotePaperMapper,
+) {
+  fun voteUpdatedMessageProduce(voteResponseDTO: VoteResponseDTO, action: VoteAction) {
+    voteResponseDTOMapper.toVoteAvro(voteResponseDTO, action).let {
+      voteKafkaTemplate.send("vote", it.roomId, it)
+    }
+  }
 
+
+  fun votePaperAddedMessageProduce(votePaperDTO: VotePaperDTO) {
+    votePaperMapper.toVotePaperAvro(votePaperDTO).let {
+      votePaperKafkaTemplate.send("vote-paper", votePaperDTO.roomId, it)
+    }
+  }
 }
