68390ae04742fcdd8ff5fe640bbd7be4648ec858
minguni <80194344+min-guni@users.noreply.github.com>
2025-03-18 22:46:07 +0900

    feature : User Entity 작성

--- user-service/src/main/kotlin/vottega/user_service/domain/User.kt
+++ user-service/src/main/kotlin/vottega/user_service/domain/User.kt
@@ -1,22 +1,57 @@
 package vottega.user_service.domain
 
 import jakarta.persistence.*
+import java.time.LocalDateTime
 
 @Entity
-data class User(
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long = 0,
-
-    @Column(nullable = false, length = 15)
-    var username: String,
-
-    @Column(nullable = false, length = 15)
-    var password: String,
-
-    @Column(nullable = false, length = 30)
-    var email: String,
-
-    @Column(nullable = false, length = 10)
-    var phonenumber: String
+@Table(
+  uniqueConstraints = [
+    UniqueConstraint(columnNames = ["userId"]),
+    UniqueConstraint(columnNames = ["email"])
+  ],
+  indexes = [
+    Index(name = "idx_user_userId", columnList = "userId"),
+    Index(name = "idx_user_email", columnList = "email")
+  ]
 )
+data class User(
+  @Column(nullable = false, length = 15)
+  var username: String,
+
+  @Column(nullable = false, length = 15)
+  var userId: String,
+
+  @Column(nullable = false)
+  var password: String,
+  @Column
+  var email: String,
+) {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  var id: Long? = null
+
+  @Column
+  var createdAt: LocalDateTime? = null
+
+  @Column
+  var lastUpdatedAt: LocalDateTime? = null
+
+  fun updateName(name: String) {
+    this.username = name
+  }
+
+  fun updatePassword(password: String) {
+    this.password = password
+  }
+
+  @PrePersist
+  fun prePersist() {
+    this.createdAt = LocalDateTime.now()
+    this.lastUpdatedAt = LocalDateTime.now()
+  }
+
+  @PreUpdate
+  fun preUpdate() {
+    this.lastUpdatedAt = LocalDateTime.now()
+  }
+}
