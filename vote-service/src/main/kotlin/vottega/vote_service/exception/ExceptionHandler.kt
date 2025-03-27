7846a0667b9089f6b9b85cf0b61d1601c0a1a653
minguni <80194344+min-guni@users.noreply.github.com>
2025-03-27 21:55:57 +0900

    fix : restControllerAdvice로 변경

--- vote-service/src/main/kotlin/vottega/vote_service/exception/ExceptionHandler.kt
+++ vote-service/src/main/kotlin/vottega/vote_service/exception/ExceptionHandler.kt
@@ -1,11 +1,11 @@
 package vottega.vote_service.exception
 
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestControllerAdvice
 
-@ControllerAdvice
+@RestControllerAdvice
 class ExceptionHandler {
 
   @ExceptionHandler(VoteNotFoundException::class)
