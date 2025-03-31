b83e0cdccd206aef6751ba6de52fb8f78affad09
minguni <80194344+min-guni@users.noreply.github.com>
2025-03-31 23:43:23 +0900

    fix : controller 단순화

--- auth-service/src/main/kotlin/vottega/auth_server/web/AuthController.kt
+++ auth-service/src/main/kotlin/vottega/auth_server/web/AuthController.kt
@@ -3,63 +3,22 @@ package vottega.auth_server.web
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import reactor.core.publisher.Mono
-import vottega.auth_server.dto.AuthRequestDTO
-import vottega.auth_server.dto.AuthResponseDTO
-import vottega.auth_server.dto.VerifyRequestDTO
-import vottega.auth_server.dto.VerifyResponseDTO
+import vottega.auth_server.dto.*
 import vottega.auth_server.service.AuthService
-import java.util.*
 
 @RestController
 @RequestMapping("/auth")
 class AuthController(private val authService: AuthService) {
+  @PostMapping("/verify")
+  fun verify(@RequestBody verifyRequestDTO: VerifyRequestDTO): Mono<VerifyResponseDTO> =
+    authService.verify(verifyRequestDTO.token)
 
-    @PostMapping("/login")
-    fun login(@RequestBody authRequestDTO: AuthRequestDTO): Mono<ResponseEntity<AuthResponseDTO>> {
-        return authService.login(authRequestDTO)
-            .map { authResponseDTO -> ResponseEntity.ok(authResponseDTO) }
-            .onErrorResume { error ->
-                Mono.just(ResponseEntity.badRequest().body(null)) // null 대신 에러 메시지만 반환
-            }
-    }
+  @PostMapping("/user")
+  fun createUserToken(@RequestBody userAuthRequestDTO: UserAuthRequestDTO): Mono<AuthResponseDTO> =
+    authService.createUserToken(userAuthRequestDTO.id, userAuthRequestDTO.userId)
 
-    @PostMapping("/verify")
-    fun verify(@RequestBody verifyRequestDTO: VerifyRequestDTO): Mono<ResponseEntity<VerifyResponseDTO>> {
-        return authService.verify(verifyRequestDTO.token)
-            .map { verifyResult ->
-                ResponseEntity.ok(
-                    VerifyResponseDTO(
-                        role = verifyResult.role,
-                        participantId = verifyResult.participantId,
-                        roomId = verifyResult.roomId,
-                        userId = verifyResult.userId
-                    )
-                )
-            }
-            .onErrorResume { error ->
-                Mono.just(ResponseEntity.badRequest().body(null))
-            }
-
-    }
-
-    @GetMapping("/user/{userId}")
-    fun authenticateUserId(@PathVariable userId: UUID): Mono<ResponseEntity<String>> {
-        return authService.authenticateUserId(userId)
-            .map { token -> ResponseEntity.ok(token) }
-            .onErrorResume { error ->
-                Mono.just(ResponseEntity.badRequest().body("Error: ${error.message}"))
-            }
-    }
-
-    @PostMapping("/room-owner")
-    fun authenticateRoomOwner(
-        @RequestParam username: String,
-        @RequestParam password: String
-    ): Mono<ResponseEntity<String>> {
-        return authService.authenticateRoomOwner(username, password)
-            .map { token -> ResponseEntity.ok(token) }
-            .onErrorResume { error ->
-                Mono.just(ResponseEntity.badRequest().body("Error: ${error.message}"))
-            }
-    }
+  @PostMapping("/participant")
+  fun authenticateParticipant(
+    @RequestBody participantAuthRequestDTO: ParticipantAuthRequestDTO
+  ): Mono<AuthResponseDTO> = authService.authenticateParticipantId(participantAuthRequestDTO.participantId)
 }
