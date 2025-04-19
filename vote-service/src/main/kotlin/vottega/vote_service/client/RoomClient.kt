b26dcee52f64b02054add7804b89c393f00c76f0
minguni <80194344+min-guni@users.noreply.github.com>
2025-04-19 17:11:08 +0900

    fix : feign client fix typos

--- vote-service/src/main/kotlin/vottega/vote_service/client/RoomClient.kt
+++ vote-service/src/main/kotlin/vottega/vote_service/client/RoomClient.kt
@@ -2,12 +2,13 @@ package vottega.vote_service.client
 
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import vottega.vote_service.config.FeignConfig
 import vottega.vote_service.dto.room.RoomResponseDTO
 
 @FeignClient(name = "room-service", configuration = [FeignConfig::class])
 interface RoomClient {
-  @GetMapping("api/auth/user}")
-  fun getRoom(roomId: Long): RoomResponseDTO
+  @GetMapping("api/room/{roomId}")
+  fun getRoom(@PathVariable("roomId") roomId: Long): RoomResponseDTO
 }
 
