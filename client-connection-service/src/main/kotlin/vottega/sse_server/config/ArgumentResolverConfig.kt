b95849a0ee32a423d309c82122a5a04a80755568
minguni <80194344+min-guni@users.noreply.github.com>
2025-05-09 23:57:33 +0900

    feature : user용 argumentResolver 개발

--- client-connection-service/src/main/kotlin/vottega/sse_server/config/ArgumentResolverConfig.kt
+++ client-connection-service/src/main/kotlin/vottega/sse_server/config/ArgumentResolverConfig.kt
@@ -5,11 +5,13 @@ import RoomIdArgumentResolver
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.reactive.config.WebFluxConfigurer
 import org.springframework.web.reactive.result.method.annotation.ArgumentResolverConfigurer
+import vottega.sse_server.argumet_resolver.UserIdArgumentResolver
 
 @Configuration
 class ArgumentResolverConfig : WebFluxConfigurer {
   override fun configureArgumentResolvers(configurer: ArgumentResolverConfigurer) {
     configurer.addCustomResolver(ParticipantArgumentResolver())
     configurer.addCustomResolver(RoomIdArgumentResolver())
+    configurer.addCustomResolver(UserIdArgumentResolver())
   }
 }
