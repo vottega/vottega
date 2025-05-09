996fdaaf4b46e2a10b6b7c627677bbb72cde81f4
minguni <80194344+min-guni@users.noreply.github.com>
2025-05-09 23:56:49 +0900

    feature : roomParticipantRepository 최적화

--- client-connection-service/src/main/kotlin/vottega/sse_server/repository/RoomParticipantRepository.kt
+++ client-connection-service/src/main/kotlin/vottega/sse_server/repository/RoomParticipantRepository.kt
@@ -2,21 +2,29 @@ package vottega.sse_server.repository
 
 import org.springframework.stereotype.Repository
 import reactor.core.publisher.Mono
-import java.util.*
+import reactor.core.scheduler.Schedulers
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.CopyOnWriteArraySet
 
 @Repository
 class RoomParticipantRepository {
-  private val roomParticipantMap = mutableMapOf<Long, MutableSet<UUID>>()
+  private val roomParticipantMap =
+    ConcurrentHashMap<Long, MutableSet<Any>>()
 
-  fun addParticipant(roomId: Long, userId: UUID): Mono<MutableSet<UUID>> {
-    return Mono.fromSupplier {
-      val participants = roomParticipantMap.computeIfAbsent(roomId) { mutableSetOf() }
-      participants.add(userId)
-      participants
-    }
-  }
+  fun addParticipant(roomId: Long, userId: Any): Mono<Set<Any>> =
+    Mono.fromCallable {
+      roomParticipantMap
+        .computeIfAbsent(roomId) { CopyOnWriteArraySet() }
+        .apply { add(userId) }
+        .toSet()
+    }.subscribeOn(Schedulers.parallel())
 
-  fun removeParticipant(roomId: Long, userId: UUID): Mono<MutableSet<UUID>> {
-    return Mono.fromSupplier { roomParticipantMap[roomId]?.apply { remove(userId) } ?: mutableSetOf() }
-  }
+  fun removeParticipant(roomId: Long, userId: Any): Mono<Set<Any>> =
+    Mono.fromCallable {
+      roomParticipantMap[roomId]
+        ?.apply { remove(userId) }
+        ?.takeIf { it.isNotEmpty() }
+        ?.toSet()
+        ?: emptySet()
+    }.subscribeOn(Schedulers.parallel())
 }
