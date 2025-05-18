1b14b76c7bb9a004bae812e8b1670e15b66282ef
minguni <80194344+min-guni@users.noreply.github.com>
2025-05-18 23:34:09 +0900

    feature : oauth2 적용을 위해 implement 추가

--- user-service/build.gradle.kts
+++ user-service/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
   implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
   implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+  implementation("org.springframework.security:spring-security-oauth2-client")
   implementation("org.jetbrains.kotlin:kotlin-reflect")
   implementation("org.springframework.kafka:spring-kafka")
   compileOnly("org.projectlombok:lombok")
