c2a030570e9b7ce1fe581c820eae7cf47355c1c9
minguni <80194344+min-guni@users.noreply.github.com>
2025-07-27 13:38:09 +0900

    fix : participant dto 및 mapper nullable 변경

--- vote-service/src/main/kotlin/vottega/vote_service/dto/room/ParticipantResponseDTO.kt
+++ vote-service/src/main/kotlin/vottega/vote_service/dto/room/ParticipantResponseDTO.kt
@@ -1,16 +1,17 @@
-package vottega.vote_service.dto.room
+package vottega.sse_server.dto
 
+import vottega.vote_service.dto.room.ParticipantRoleDTO
 import java.time.LocalDateTime
-import java.util.UUID
+import java.util.*
 
 data class ParticipantResponseDTO(
-    val id: UUID,
-    val name: String,
-    val roomId: Long,
-    val position: String?,
-    val participantRole: ParticipantRoleDTO,
-    val isEntered: Boolean,
-    val createdAt: LocalDateTime,
-    val enteredAt: LocalDateTime?,
-    val lastUpdatedAt: LocalDateTime,
+  val id: UUID,
+  val name: String?,
+  val roomId: Long,
+  val position: String?,
+  val participantRole: ParticipantRoleDTO?,
+  val isEntered: Boolean?,
+  val createdAt: LocalDateTime,
+  val enteredAt: LocalDateTime?,
+  val lastUpdatedAt: LocalDateTime?,
 )
